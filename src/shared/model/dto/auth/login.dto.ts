02e2d65f23db55d5c1095052fc569f7c90f106db
psobhanlo <psobhanlo@gmail.com>
2025-09-06 08:25:55 +0330

    footer

--- src/shared/model/dto/auth/login.dto.ts
+++ src/shared/model/dto/auth/login.dto.ts
@@ -1,15 +1,15 @@
 import {BaseEntity} from "@/shared/model/base/base-entity";
 import {EN_RoleEnum} from "@/shared/enum/form/EN_Role.enum";
 
-export class LoginDto extends BaseEntity{
-    phoneNumber:string;
-    password:string;
-    token:string;
-    fullName:string;
-    code:number;
-    firstname:string;
-    lastname:string;
-    role: EN_RoleEnum;
+export class LoginDto extends BaseEntity {
+    phoneNumber: string | number | undefined;
+    password: string | undefined;
+    token: string | undefined;
+    fullName: string;
+    code: number | undefined;
+    firstname: string | undefined;
+    lastname: string | undefined;
+    role: EN_RoleEnum | undefined;
     provinceId: number;
     cityId: number;
 }
